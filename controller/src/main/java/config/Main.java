7edf1e69149aee8a9176493a5ed20a00d856105d
celfons <marcel.resende@yahoo.com.br>
2018-11-26 10:09:52 -0200

    added log4j

--- controller/src/main/java/config/Main.java
+++ controller/src/main/java/config/Main.java
@@ -1,13 +1,18 @@
 package config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        LOGGER.info("Debug Message Logged !!!");
     }
 
 }
